5deee4498737487a64c14cdd0081c5e3df52a4b9
Monica Liu <liuqianmonica@gmail.com>
2023-01-31 21:13:54 -0800

    graph dfs

--- Graph/200_NumberofIslands.py
+++ Graph/200_NumberofIslands.py
@@ -25,3 +25,31 @@ class Solution:
         return count
     # time: o(m*n)
     # space: o(m*n)
+
+    solution 2:
+
+    class Solution:
+        def numIslands(self, grid: List[List[str]]) -> int:
+            seen = set()
+            count = 0
+
+            def dfs(i, j):
+                direction = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+                if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or (i, j) \
+                        in seen or grid[i][j] == '0':
+                    return
+                if (i, j) not in seen:
+                    seen.add((i, j))
+                for row, col in direction:
+                    dfs(i + row, j + col)
+
+            for i in range(len(grid)):
+                for j in range(len(grid[0])):
+                    if grid[i][j] == '1' and (i, j) not in seen:
+                        # seen.add(grid[i][j])
+                        count += 1
+                        dfs(i, j)
+
+            return count
+        # time: o(m*n)
+        # space: o(m*n)
